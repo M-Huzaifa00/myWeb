1d6468e643f55a3215bb4f7a5bd41364731b4ef8
M-Huzaifa00 <121715060+M-Huzaifa00@users.noreply.github.com>
2023-07-20 07:32:05 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,41 +1,61 @@
 const express = require("express");
-const https = require("https")
-const app = express()
+const https = require("https");
+const bodyParser = require("body-parser");
+const app = express();
 
-app.get("/",(req,res)=>{
-    let url = "https://api.openweathermap.org/data/2.5/weather?q=Chakwal&units=metric&appid=a9ef4dbd5339379386fc758b4740a1e9";
-    https.get(url, (response)=>{
-      console.log(response.statusCode);
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-      response.on("data",(data)=>{
-        const weatherData = JSON.parse(data)
-        let temprature = weatherData.main.temp;
-        let description = weatherData.weather[0].description;
-        let icon =  weatherData.weather[0].icon;
-        let imgSrc =`https://openweathermap.org/img/wn/${icon}@2x.png`
-        res.write("<p>The weather is currently  " + description + "</p>")
-        res.write("<h1>The temp in Chakwal is : " + temprature + " celcius.</h1>")
-        res.write(`<img src="${imgSrc}">`) 
-        res.send();
+app.post("/", (req, res) => {
+  let firstName = req.body.fname;
+  let lastName = req.body.lname;
+  let email = req.body.email;
 
-      })
-    })
-})
+  let data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+  let jsonData = JSON.stringify(data);
 
+  let options = {
+    method: "POST",
+    auth: "huzaifa:8267bb212ab7321b243826aaa05d585b-us21",
+  };
 
+  app.post("/fail",(req,res)=>{
+    res.redirect("/");
+  })
 
+  const url = `https://us21.api.mailchimp.com/3.0/lists/876ae7f5ac`;
 
+  const mailchimpRequest = https.request(url, options, (response) => {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/fail.html");
+    }
 
+    response.on("data", (data) => {
+      console.log(JSON.parse(data));
+    });
+  });
 
+  mailchimpRequest.write(jsonData);
+  mailchimpRequest.end();
+});
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
-
-
-
-
-
-
-
-app.listen(3000,()=>{
-    console.log("Server started at local host : 3000");
-})
+app.listen(3000, () => {
+  console.log("Server started at port: 3000");
+});
